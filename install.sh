e0ec59870e72f26e266b5bcb4e500b8e1c5716bd
Ent0248 <1563914089@qq.com>
2025-02-23 20:05:48 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 cp -r .config/anusb "/data/user/0/com.termux/files/home/.config/anusb"
 
-cp anusb.sh /data/user/0/com.termux/files/usr/bin/anusb
+cp anusb.sh /data/user/0/com.termux/files/usr/bin/anusb.sh
+
+
+cp anusb /data/user/0/com.termux/files/usr/bin/anusb
 
 chmod 777 /data/user/0/com.termux/files/usr/bin/anusb
